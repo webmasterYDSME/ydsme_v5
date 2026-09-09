963ce9a4fe9b54cf95f208b57e2464f57b8641a7
Nomen Ama <83960342+nomenama@users.noreply.github.com>
2026-09-09 23:28:00 +0100

    ci skip for deployment fix [skip ci]

--- app/under-review/page.tsx
+++ app/under-review/page.tsx
@@ -2,8 +2,12 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "404",
-  description:
-    "Not found",
+  description: "Website under review - not found.",
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+  },
 };
 
 export default function UnderReviewPage() {
@@ -19,16 +23,16 @@ export default function UnderReviewPage() {
         padding: "48px 16px",
       }}
     >
-        <h1
-          style={{
-            fontSize: "45px",
-            fontWeight: 400,
-            lineHeight: 1.25,
-            marginTop: 0,
-          }}
-        >
-          404 Not Found
-        </h1>
+      <h1
+        style={{
+          margin: 0,
+          fontSize: "45px",
+          fontWeight: 400,
+          lineHeight: 1.25,
+        }}
+      >
+        404 Not Found
+      </h1>
     </main>
   );
 }
